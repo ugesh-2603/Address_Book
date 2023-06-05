db45052f9a51e8864ad5bb5ec7f442d69b334e0d
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-06-05 08:33:33 +0530

    UC1

---
+++ Address_Book/Contacts.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book
+{
+    internal class Contacts
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Present_Address { get; set; }
+
+        public string Permanent_Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public Contacts(string firstName, string lastName, string Present_address, string Permanent_address, string city, string state, string zip, string phoneNumber, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Present_Address = Present_address;
+            Permanent_Address = Permanent_address;
+            City = city;
+            State = state;
+            Zip = zip;
+            PhoneNumber = phoneNumber;
+            Email = email;
+
+        }
+    }
+}
--- Address_Book/Program.cs
+++ Address_Book/Program.cs
@@ -5,6 +5,37 @@
         static void Main(string[] args)
         {
             Console.WriteLine(" Welcome to Address Book Problem ");
+
+            Console.Write("Enter First Name: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Enter Present_Address: ");
+            string Present_address = Console.ReadLine();
+
+            Console.Write("Enter Premanent_Address: ");
+            string Permanent_address = Console.ReadLine();
+
+            Console.Write("Enter City: ");
+            string city = Console.ReadLine();
+
+            Console.Write("Enter State: ");
+            string state = Console.ReadLine();
+
+            Console.Write("Enter Zip Code: ");
+            string zip = Console.ReadLine();
+
+            Console.Write("Enter Phone Number: ");
+            string phoneNumber = Console.ReadLine();
+
+            Console.Write("Enter Email: ");
+            string email = Console.ReadLine();
+
+            Contacts contact = new Contacts(firstName, lastName, Present_address, Permanent_address , city, state, zip, phoneNumber, email);
+
+            Console.WriteLine($"Created contact: {contact.FirstName} {contact.LastName}");
         }
     }
 }
