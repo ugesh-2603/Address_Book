4ddf1f89b682e1a9aaf93cf66f67f0ee12023d00
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-06-07 12:43:17 +0530

    UC5_Multiple_Contacts

--- Address_Book/Program.cs
+++ Address_Book/Program.cs
@@ -8,6 +8,38 @@
 
             AddressBook addressBook = new AddressBook();
 
+            while (true)
+            {
+                Console.WriteLine("Please select an option:");
+                Console.WriteLine("1. Create Contact");
+                Console.WriteLine("2. Edit Contact");
+                Console.WriteLine("3. Delete Contact");
+                Console.WriteLine("4. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        CreateContact(addressBook);
+                        break;
+                    case 2:
+                        EditContact(addressBook);
+                        break;
+                    case 3:
+                        DeleteContact(addressBook);
+                        break;
+                    case 4:
+                        return;
+                    default:
+                        Console.WriteLine("Invalid option. Please select again.");
+                        break;
+                }
+            }
+        }
+
+        static void CreateContact(AddressBook addressBook)
+        {
+
             Console.Write("Enter First Name: ");
             string firstName = Console.ReadLine();
 
@@ -35,72 +67,38 @@
             Console.Write("Enter Email: ");
             string email = Console.ReadLine();
 
-            Contacts contact = new Contacts(firstName, lastName, Present_address, Permanent_address , city, state, zip, phoneNumber, email);
-
+            Contacts contact = new Contacts(firstName, lastName, Present_address, Permanent_address, city, state, zip, phoneNumber, email);
             addressBook.AddContact(contact);
 
-            Console.WriteLine($"Created contact: {contact.FirstName} {contact.LastName}");
-
-            Console.WriteLine("Do you want to edit a contact? (yes/no)");
-            string editInput = Console.ReadLine();
-
-            if (editInput.ToLower() == "yes")
-            {
-                Console.Write("Enter First Name of contact to edit: ");
-                string firstNameToEdit = Console.ReadLine();
-
-                Console.Write("Enter Last Name of contact to edit: ");
-                string lastNameToEdit = Console.ReadLine();
-
-                Console.WriteLine("Enter new contact details:");
-
-                Console.Write("Enter First Name: ");
-                 firstName = Console.ReadLine();
-
-                Console.Write("Enter Last Name: ");
-                 lastName = Console.ReadLine();
-
-                Console.Write("Enter Present_Address: ");
-                 Present_address = Console.ReadLine();
-
-                Console.Write("Enter Premanent_Address: ");
-                 Permanent_address = Console.ReadLine();
-
-                Console.Write("Enter City: ");
-                 city = Console.ReadLine();
-
-                Console.Write("Enter State: ");
-                 state = Console.ReadLine();
-
-                Console.Write("Enter Zip Code: ");
-                 zip = Console.ReadLine();
-
-                Console.Write("Enter Phone Number: ");
-                 phoneNumber = Console.ReadLine();
-
-                Console.Write("Enter Email: ");
-                 email = Console.ReadLine();
-
-                Contacts newcontact = new Contacts(firstName, lastName, Present_address, Permanent_address, city, state, zip, phoneNumber, email);
-
-                addressBook.EditContact(firstNameToEdit, lastNameToEdit, newcontact);
-
-                Console.WriteLine($"Edited contact: {newcontact.FirstName} {newcontact.LastName}");
-            }
-
-            Console.WriteLine("Do you want to delete a contact? (yes/no)");
-            string deleteInput = Console.ReadLine();
-
-            if (deleteInput.ToLower() == "yes")
-            {
-                Console.Write("Enter First Name of contact to delete: ");
-                string firstNameToDelete = Console.ReadLine();
-
-                Console.Write("Enter Last Name of contact to delete: ");
-                string lastNameToDelete = Console.ReadLine();
-
-                addressBook.DeleteContact(firstNameToDelete, lastNameToDelete);
-            }
+            Console.WriteLine($"Added contact: {contact.FirstName} {contact.LastName}");
         }
+
+        static void EditContact(AddressBook addressBook)
+        {
+            Console.Write("Enter First Name of contact to edit: ");
+            string firstNameToEdit = Console.ReadLine();
+
+            Console.Write("Enter Last Name of contact to edit: ");
+            string lastNameToEdit = Console.ReadLine();
+
+            Console.WriteLine("Enter new contact details:");
+            CreateContact(addressBook);
+
+            Console.WriteLine($"Edited contact: {firstNameToEdit} {lastNameToEdit}");
+        }
+
+        static void DeleteContact(AddressBook addressBook)
+        {
+            Console.Write("Enter First Name of contact to delete: ");
+            string firstNameToDelete = Console.ReadLine();
+
+            Console.Write("Enter Last Name of contact to delete: ");
+            string lastNameToDelete = Console.ReadLine();
+
+            addressBook.DeleteContact(firstNameToDelete, lastNameToDelete);
+
+            Console.WriteLine($"Deleted contact: {firstNameToDelete} {lastNameToDelete}");
+        }
+
     }
 }
