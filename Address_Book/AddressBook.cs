10835969ad7f62c26d57896606c9657ccfab6aed
ugesh-2603 <75109057+ugesh-2603@users.noreply.github.com>
2023-06-11 18:09:45 +0530

    Update AddressBook.cs

--- Address_Book/AddressBook.cs
+++ Address_Book/AddressBook.cs
@@ -8,16 +8,18 @@ namespace Address_Book
 {
     internal class AddressBook
     {
-        public List<Contacts> Contacts { get; set; }
+        public List<Contacts> Contacts { get; set; } // Declaration of a public property named "Contacts" of type List<Contacts>.
+                                                     // This property will be used to store a collection of contacts.
 
-        public AddressBook()
+        public AddressBook()  // Constructor for the AddressBook class.
         {
-            Contacts = new List<Contacts>();
+            Contacts = new List<Contacts>(); // Initializes the "Contacts" property as a new instance of List<Contacts>.
         }
 
-        public void AddContact(Contacts contact)
+        public void AddContact(Contacts contact) // Method for adding a contact to the address book.
+                                                // Takes a parameter named "contact" of type Contacts, representing the contact to be added.
         {
-            Contacts.Add(contact);
-        }
+            Contacts.Add(contact);   // Adds the specified contact to the "Contacts" list.
+        } 
     }
 }
